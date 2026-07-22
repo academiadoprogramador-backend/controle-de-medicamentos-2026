f0d3d6aeff86edb0492503fae5463f1872218252
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2026-07-21 21:15:47 -0300

    Adiciona exclusão de fornecedores

--- ControleDeMedicamentos.WebApp/ModuloFornecedores/FornecedorController.cs
+++ ControleDeMedicamentos.WebApp/ModuloFornecedores/FornecedorController.cs
@@ -54,16 +54,34 @@ public sealed class FornecedorController : Controller
     [HttpPost]
     public ActionResult Editar(int id, string nome, string telefone, string cnpj)
     {
-        Fornecedor? fornecedor = repositorio.SelecionarPorId(id);
-
-        if (fornecedor == null)
-            return NotFound();
-
         Fornecedor fornecedorAtualizado = new Fornecedor(nome, telefone, cnpj);
 
         bool conseguiuEditar = repositorio.Editar(id, fornecedorAtualizado);
 
         if (!conseguiuEditar)
+            return NotFound();
+
+        return RedirectToAction(nameof(Listar));
+    }
+
+    [HttpGet]
+    public ActionResult Excluir(int id)
+    {
+        Fornecedor? fornecedor = repositorio.SelecionarPorId(id);
+
+        if (fornecedor == null)
+            return NotFound();
+
+        return View(fornecedor);
+    }
+
+    [HttpPost]
+    [ActionName("Excluir")]
+    public ActionResult ConfirmarExclusao(int id)
+    {
+        bool conseguiuExcluir = repositorio.Excluir(id);
+
+        if (!conseguiuExcluir)
             return NotFound();
 
         return RedirectToAction(nameof(Listar));
