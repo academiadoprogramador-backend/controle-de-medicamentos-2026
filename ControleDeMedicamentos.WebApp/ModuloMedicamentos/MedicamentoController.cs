73b612edbbb824b5bff7d366e2dabedd5c92bb8b
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2026-07-23 20:29:41 -0300

    Corrige fornecedores faltando durante Cadastro de Medicamento

--- ControleDeMedicamentos.WebApp/ModuloMedicamentos/MedicamentoController.cs
+++ ControleDeMedicamentos.WebApp/ModuloMedicamentos/MedicamentoController.cs
@@ -30,6 +30,10 @@ public sealed class MedicamentoController : Controller
     [HttpGet]
     public ActionResult Cadastrar()
     {
+        List<Fornecedor> fornecedores = repositorioFornecedor.SelecionarTodos();
+
+        ViewBag.Fornecedores = fornecedores;
+
         return View();
     }
 
